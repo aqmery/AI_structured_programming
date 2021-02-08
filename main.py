f5e057e54cb25af0d60cf23f391771c64afa1b1b
aqmery <henrydelange8412@gmail.com>
2021-02-08 14:54:52 +0100

    updated main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 from constants import AMOUNT_TO_GUESS, AMOUNT_OF_TRIES, COLORLST
+"""Master mind game, constant variables are located in the file "constants.py" 
+the function "Gameloop" starts the game """
 
-
-def mastercode(COLORLST):
+def MasterCode(COLORLST):
     """generates the code that the user needs to guess"""
     master = []
     for i in range(0, AMOUNT_TO_GUESS):
@@ -12,7 +13,7 @@ def mastercode(COLORLST):
     return master
 
 
-def blackcheck(code, guesslst):
+def BlackCheck(code, guesslst):
     """checks if there are any guesses in the right place (making them black pins)
      and also calls the check for the white pins"""
     mastercopy = code.copy()
@@ -20,9 +21,13 @@ def blackcheck(code, guesslst):
     for i in range(0, len(guesslst)):
         if guesslst[i] == mastercopy[i]:
             black_dots += 1
-    whitecheck(mastercopy, guesslst, black_dots)
+    WhiteCheck(mastercopy, guesslst, black_dots)
+    if black_dots == 4:
+        return "you won the game!!"''
+    else:
+        return f"amount of black dots: {black_dots}"
 
-def whitecheck(mastercopy, guesslst, black_dots):
+def WhiteCheck(mastercopy, guesslst, black_dots):
     """the check for the white pins, checks if there are pins in the code that are not in the right spot,
      and returns the amount of the incorrect pins"""
     white_dots = 0
@@ -34,19 +39,19 @@ def whitecheck(mastercopy, guesslst, black_dots):
 
 
 def GameLoop():
-    """starts the game and keeps it going until the code is guessed or the tries run out"""
-    code = mastercode(COLORLST)
+    """starts the game and keeps it going until the code is guessed or the tries run out.
+    https://www.tutorialspoint.com/python-get-a-list-as-input-from-user this discribes the:
+    list(map(str, input("make a guess: ").strip().split(',')))[:AMOUNT_TO_GUESS] function.
+    """
+    code = MasterCode(COLORLST)
     tries = 0
-    while tries < AMOUNT_OF_TRIES:
+    stop = False
+    while tries < AMOUNT_OF_TRIES or stop == True:
         guesslst = list(map(str, input("make a guess: ").strip().split(',')))[:AMOUNT_TO_GUESS]
+        print(guesslst)
         tries += 1
-        black = blackcheck(code,guesslst)
-        if black == 4:
-            return "you won the game!!"''
-        else:
-            f"amount of black dots: {black}"
-            print(f"you have {AMOUNT_OF_TRIES - tries} tries left \n")
-
+        print(BlackCheck(code,guesslst))
+        print(f"you have {AMOUNT_OF_TRIES - tries} tries left \n")
 
 
 GameLoop()
